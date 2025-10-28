c3a1c97954e7def0f68ce3aca8ac814319b8b7ed
Muhammad Haseeb <muhammadhaseeb@crunchcraft.com>
2025-10-28 15:03:11 +0500

    slider issue has been fixed, changed the slider to swiper

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,80 +1,59 @@
 import React, { useContext, useState, useEffect } from "react";
-import Slider from "react-slick";
 import { DataContext } from "../../App";
 import ProductDetailsPopup from "../ProductDetailsPopup/ProductDetailsPopup";
-import styles from "./Featured.module.css"
+import styles from "./Featured.module.css";
 import { handleAddToCart } from "../handleCart/handleAddToCart";
 import { Link } from "react-router";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
 
 function Featured() {
-  const { sharedData, cartItems, setCartCount, setCartItems } = useContext(DataContext);
+  const { sharedData, cartItems, setCartCount, setCartItems } =
+    useContext(DataContext);
   const [detailsModal, showDetailsModal] = useState(false);
   const [previewItem, setPreviewItem] = useState();
-   const [sliderKey, setSliderKey] = useState(0);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setSliderKey(prev => prev + 1);
-    };
-
-    window.addEventListener('resize', handleResize);
-    
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-
-
 
   const handlePreview = (item) => {
     showDetailsModal(true);
     setPreviewItem(item);
   };
 
-
-
-
-
-  var settings = {
-    dots: false,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    // initialSlide: 0,
-    arrows: true,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          // slidesToScroll: 3,
-          // infinite: true,
-          // dots: true,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          // arrows: false,
-          // slidesToScroll: 2,
-          // initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 575,
-        settings: {
-          slidesToShow: 1,
-          // slidesToScroll: 1,
-        },
-      },
-    ],
-  };
-
-
-
+  // var settings = {
+  //   dots: false,
+  //   infinite: false,
+  //   speed: 500,
+  //   slidesToShow: 4,
+  //   slidesToScroll: 1,
+  //   // initialSlide: 0,
+  //   arrows: true,
+  //   responsive: [
+  //     {
+  //       breakpoint: 1024,
+  //       settings: {
+  //         slidesToShow: 3,
+  //         // slidesToScroll: 3,
+  //         // infinite: true,
+  //         // dots: true,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 768,
+  //       settings: {
+  //         slidesToShow: 2,
+  //         // arrows: false,
+  //         // slidesToScroll: 2,
+  //         // initialSlide: 2,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 575,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         // slidesToScroll: 1,
+  //       },
+  //     },
+  //   ],
+  // };
 
   return (
     <>
@@ -85,10 +64,10 @@ function Featured() {
         />
       )}
 
-      <section className={` my-5 featuredWrap ${styles.featuredWrap}`} >
+      <section className={` my-5 featuredWrap ${styles.featuredWrap}`}>
         <div className="container">
-          <h2 className="text-center mb-5" >Featured Products</h2>
-          <Slider key={sliderKey} {...settings}>
+          <h2 className="text-center mb-5">Featured Products</h2>
+          {/* <Slider  {...settings}>
             {sharedData?.map((item) => (
               <div key={item.id} className="productCard my-3">
                 <div className="productImg d-flex align-items-center justify-content-center ">
@@ -122,7 +101,63 @@ function Featured() {
                 <span className="productPrice">${item.price}</span>
               </div>
             ))}
-          </Slider>
+          </Slider> */}
+          <Swiper 
+          modules={[Navigation, Pagination]}
+          slidesPerView={1}
+           navigation
+           breakpoints={{
+            1200: { slidesPerView: 4 },
+            992: { slidesPerView: 3 },
+            // 768: { slidesPerView: 3 },
+            575: { slidesPerView: 2 },
+          }}
+          >
+            {sharedData?.map((item) => (
+              <SwiperSlide key={item.id}>
+                <div className="productCard my-3">
+                  <div className="productImg d-flex align-items-center justify-content-center ">
+                    <div>
+                      <img
+                        src={item.image}
+                        width={300}
+                        height={150}
+                        alt="Demo Store"
+                      />
+                    </div>
+                    <div
+                      className="cardPreviewHover"
+                      onClick={() => handlePreview(item)}
+                    >
+                      <i className="fa-solid fa-eye"></i>
+                      <span>Quick Preview</span>
+                    </div>
+                  </div>
+                  <div className="d-flex justify-content-between align-items-center mt-3">
+                    <h3 className="m-0">
+                      <Link to={`/ProductDetailsPage/${item.id}`}>
+                        <span className="productName">{item.title}</span>
+                      </Link>
+                    </h3>
+                    <span
+                      className="cartPlus"
+                      onClick={() =>
+                        handleAddToCart(
+                          item,
+                          cartItems,
+                          setCartCount,
+                          setCartItems
+                        )
+                      }
+                    >
+                      +
+                    </span>
+                  </div>
+                  <span className="productPrice">${item.price}</span>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </section>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-
 import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 import App from "./App.jsx";
